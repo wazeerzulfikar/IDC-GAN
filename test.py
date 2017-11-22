85f41a53a01a480e62468da9070cdb202c5d2e7a
Wazeer Zulfikar <wazeer.zulfikar@gmail.com>
2017-11-22 19:07:36 +0530

    testing time

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
+import tensorflow as tf
 
 from PIL import Image
 
@@ -10,29 +11,21 @@ import os
 from model import *
 from losses import *
 
-
-
 # load json and create model
-json_file = open('models/generator.json', 'r')
+json_file = open('/saved/generator.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 
 generator = model_from_json(loaded_model_json)
 # load weights into new model
-generator.load_weights("models/generator_90.h5")
+generator.load_weights("/saved/generator_90.h5")
 print("Loaded model from disk")
 
-rain_data_path = "dataset/rain_modified"
-derain_data_path = "dataset/derain_modified"
-
-
-def log10(x):
-  numerator = K.log(x)
-  denominator = K.log(K.constant(10, dtype=numerator.dtype))
-  return numerator / denominator
+rain_data_path = "/dataset/rain_modified"
+derain_data_path = "/dataset/derain_modified"
 
 def load_images(folder):
-	check = 1
+	check = 0
 
 	images = []
 	for filename in os.listdir(folder):
@@ -53,19 +46,21 @@ print("Rain Data Loaded.")
 derain_data = load_images(derain_data_path)
 print("DeRain Data Loaded.")
 
-print(generator.summary())
-
-
-input_images = rain_data[0:10]
-derain_images = derain_data[0:10]
+input_images = rain_data
+derain_images = derain_data
 
 generated_images = generator.predict(input_images)
 
+generated_images += 1
+generated_images *=127.5
 
-mse = generator_l2_loss(derain_images, generated_images)
-psnr = 20*log10(255/(mse**(1/2.0)))
+mse = ((rain_data - generated_images) ** 2).mean(axis=None)
+psnr = 20*np.log10(255/(mse**(1/2.0)))
 
-print("Peak Signal to Noise Ratio:", psnr)
+
+print("Peak Signal to Noise Ratio for 0:", psnr)
+
+
 
 
 
